7e22db311f24b666a27e68677dcb42e93d830a84
Biniyam Lemma <lemmasbini@gmai.com>
2022-04-01 23:53:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+    <div>
+      <grid
+        v-for="(guess, i) in state.guesses"
+        :key="i"
+        :value="guess"
+        :solution="state.solution"
+        :submitted="i < state.currentGuessIndex"
+      />
+    </div>
+    <modal
+    v-if="wonGame"
+    v-show="!isModalVisble"
+    result= " Congrats you Win!"
+     />
+     <modal 
+     v-else-if="lostGame"
+     v-show="!isModalVisble"
+     result="Lost"
+     />
+    <simple-keyboard
+      @onKeyPress="handleInput"
+      :guessedLetters="state.guessedLetters"
+    />
+  </div>
+</template>
+
+<script setup>
+import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import Grid from "./components/Grid.vue";
+import { reactive, onMounted, computed } from "vue";
+import Modal from "./components/Modal.vue";
+// import { common } from "./word/words";
+const isModalVisble = false;
+const closeModal = () => {
+      this.isModalVisble = false;
+    }
+const state = reactive({
+  solution: "benji",
+  guesses: ["", "", "", "", "", ""],
+  currentGuessIndex: 0,
+  guessedLetters: {
+    miss: [],
+    found: [],
+    hint: [],
+  },
+});
+// const getRandomWord = () => {
+//   return solution[Math.floor(Math.random() * solution.length)];
+// };
+
+// const word = ref(getRandomWord());
+
+const wonGame = computed(
+  () => state.guesses[state.currentGuessIndex - 1] === state.solution
+);
+const lostGame = computed(() => !wonGame.value && state.currentGuessIndex >= 6);
+
+const handleInput = (key) => {
+  if (state.currentGuessIndex >= 6 || wonGame.value) {
+    return;
+  }
+  const currentGuess = state.guesses[state.currentGuessIndex];
+  if (key == "{enter}") {
+    // SEND GUESS
+    if (currentGuess.length == 5) {
+      state.currentGuessIndex++;
+      setTimeout(() => {
+        for (var i = 0; i < currentGuess.length; i++) {
+          let c = currentGuess.charAt(i);
+          if (c == state.solution.charAt(i)) {
+            state.guessedLetters.found.push(c);
+          } else if (state.solution.indexOf(c) != -1) {
+            state.guessedLetters.hint.push(c);
+          } else {
+            state.guessedLetters.miss.push(c);
+          }
+        }
+      }, 2500);
+    }
+  } else if (key == "{bksp}") {
+    // REMOVE LAST LETTER
+    state.guesses[state.currentGuessIndex] = currentGuess.slice(0, -1);
+  } else if (currentGuess.length < 5) {
+    // ADD LETTER IF ALPHABETICAL
+    const alphaRegex = /[a-zA-Z]/;
+    if (alphaRegex.test(key)) {
+      state.guesses[state.currentGuessIndex] += key;
+    }
+  }
+};
+
+// const reloadPage = () => {
+//   window.location.reload();
+// };
+onMounted(() => {
+  window.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    let key =
+      e.keyCode == 13
+        ? "{enter}"
+        : e.keyCode == 8
+        ? "{bksp}"
+        : String.fromCharCode(e.keyCode).toLowerCase();
+    handleInput(key);
+  });
+});
+</script>
